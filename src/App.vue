b6fdbb45d186bf47a84a3c963003224d0b228168
VigilBushido <vigilbushido@gmail.com>
2021-10-16 00:03:34 -0400

    Vacation Card using Slots

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Card>
+      <template #image>
+        <img src="https://picsum.photos/id/1015/300" />
+      </template>
+      <template #title>
+        <h2>Sending all my love from Vacation!</h2>
+      </template>
+      <template #description>
+        <p>Please send someone to resuce me.</p>
+        <p>Lost at Sea in Paradise!</p>
+      </template>
+    </Card>
+  </div>
+</template>
+<script>
+import Card from "./components/Card.vue";
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
